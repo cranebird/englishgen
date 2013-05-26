9b3fab80f5aac4d65e0496675c8535d92dda880d
cranebird <quasi@kc4.so-net.ne.jp>
2013-05-26 17:07:45 +0900

    *** empty log message ***

--- cons.hs
+++ cons.hs
@@ -1,21 +1,36 @@
 ----------------------------------------------------------------
--- cons - QuickCheck Example
--- Generate a random sentence. Use Lisp List Version.
+-- cons.hs - QuickCheck Example
+-- Use Lisp List as Tree ver.
 -- Copyright (C) 2013 by @quasicrane (Twitter)
 ----------------------------------------------------------------
-module EnglishRule1 where
+-- | "Paradigms of Artificial Intelligence Programming: Case Studies in Common Lisp"
+-- Random English phrase generator in "2.3 A Rule-Based Solution" in Haskell.
+module EnglishGen where
 import Test.QuickCheck
 import Control.Monad
 import Data.List
 
-data Object = Nil | S String | (:.) {car, cdr :: Object} deriving (Eq)
+data Object
+    = Nil -- ^ nil Object
+    | S String -- ^ Symbol
+    | Object :. Object -- ^ Cons
+    deriving (Eq)
 
 instance Show Object where
     show Nil = "()"
     show (S x) = x
     show (x :. y) = showCons (x :. y)
 
--- see HyperSpec "22.1.3.5 Printing Lists And Conses"
+-- | Show Cons. See HyperSpec "22.1.3.5 Printing Lists And Conses"
+--
+-- >>> cons (S "x") Nil
+-- (x)
+-- >>> cons (S "x") (S "y")
+-- (x . y)
+-- >>> cons (S "a") (cons (S "x") (S "y"))
+-- (a x . y)
+-- >>> cons (S "x") (cons (S "y") Nil)
+-- (x y)
 showCons x = step1 x ""
     where
       step1 x res = step2 x (res ++ "(")
@@ -32,84 +47,97 @@ showCons x = step1 x ""
                         step5 x res
       step5 x res = res ++ ")"
 
--- list operator
+-- | Create a cons.
+--
+-- >>> cons (S "a") (S "b")
+-- (a . b)
 cons = (:.)
+-- | Create a list.
+--
+-- >>> list1 (S "x")
+-- (x)
 list1 x = x :. Nil
 list2 x y = x :. (y :. Nil)
 list3 x y z = x :. (y :. (z :. Nil))
 list4 w x y z = w :. (x :. (y :. (z :. Nil)))
-makeList = foldr cons Nil
-
+-- | Return True if object is a cons. Otherwise, return False.
 consp Nil = False
 consp (S _) = False
 consp (_ :. _) = True
-
+-- | Return True if object is Nil or symbol. Otherwise, return False.
 atom Nil = True
 atom (S _) = True
 atom (_ :. _) = False
-
+-- | Return True if object is consp or Nil. Otherwise, return False.
 listp x = consp x || x == Nil
 
--- car (x :. _) = x
--- car x | not (consp x) = error ("car expect cons but got: " ++ show x)
+-- | Return car part of cons.
+car (x :. _) = x
+car x | not (consp x) = error ("car expect cons but got: " ++ show x)
 
--- cdr (_ :. y) = y
--- cdr Nil = Nil
--- cdr x | not (consp x) = error ("cdr expect cons but got: " ++ show x)
-
+-- | Return cdr part of cons.
+cdr (_ :. y) = y
+cdr Nil = Nil
+cdr x | not (consp x) = error ("cdr expect cons but got: " ++ show x)
+-- | Return car of car of an object.
 caar x = car (car x)
-cadr x = car (cdr x)
-cddr x = cdr (cdr x)
-
+-- | Return the first cons in alist whose car equal item, or nil if no such cons is found.
 assoc :: Object -> Object -> Object
 assoc item alist
     | alist == Nil = Nil
     | not (consp alist) = error ("assoc expect list but got: " ++ show alist)
-    | not (consp (car alist)) = error ("assoc expect cons but got: " ++ show (car alist))
+    | not (consp (car alist)) =
+        error ("assoc expect cons but got: " ++ show (car alist))
     | caar alist == item = car alist
     | caar alist /= item = assoc item (cdr alist)
 
--- | Utility. Generate Lisp List from Haskell Array
+-- | Generate Lisp List from Haskell Array.
 array2list = foldr cons Nil
--- | Utility. Generate Haskell List from Lisp Array
+-- | Generate Haskell List from Lisp Array.
 list2array Nil = []
 list2array (x :. y) 
     | listp y = x : list2array y
     | otherwise = error ("list2array expect list, but got: " ++ show y)
 
--- FIXME use apply
+-- | Return list concatenation of lists.
+--
+-- >>> append (list3 (S "a") (S "b") (S "c")) (list2 (S "x") (S "y"))
+-- (a b c x y)
 append x y
     | x == Nil = y
     | otherwise = cons (car x) (append (cdr x) y)
-
--- foldr :: (a -> b -> b) -> b -> [a] -> b
--- foldl :: (a -> b -> a) -> a -> [b] -> a
--- map :: (a -> b) -> [a] -> [b]
--- mapcar :: (Object -> Object) -> Object -> Object
--- mapcar' :: Monad m => (Object -> m Object) -> Object -> m Object
--- (>>=) :: Monad m => m a -> (a -> m b) -> m b
-
+-- | Mapping operation. apply function f to elements of list.
 mapcar f list
     | list == Nil = Nil
     | otherwise = cons (f (car list)) (mapcar f (cdr list))
 
--- mapcar' :: Monad m => (Object -> m Object) -> Object -> m Object
+-- | Mapping operation. apply function f to elements of list.
+-- like mapcar, but function f is monadic.
 mapcar' f list
     | list == Nil = return Nil
     | otherwise = liftM2 cons (f (car list)) (mapcar' f (cdr list))
 
+-- | Apply fn to each element of list and append the results.
 mappend :: (Object -> Object) -> Object -> Object
 mappend f list
     | list == Nil = Nil
     | otherwise = append (f (car list)) (mappend f (cdr list))
 
 -- | Apply fn to each element of list and append the results.
+-- like mappend, but function f is monadic.
 mappend' f list
     | list == Nil = return Nil
     | otherwise = liftM2 append (f (car list)) (mappend' f (cdr list)) 
 
+-- | Grammar as alist.
+type Grammar = Object
 -- | Generate a random sentence or phrase.
-generate :: Object -> Object -> Gen Object
+--
+-- >>> sample' $ generate simpleGrammar (S "noun-phrase")
+-- [(a woman),(a woman),(the ball),(the ball),(the man),(the table),(a ball),(a ball),(a table),(a man),(the man)]
+generate :: Grammar
+         -> Object
+         -> Gen Object
 generate grm phrase
     | listp phrase = mappend' g phrase
     | rewrites grm phrase /= Nil =
@@ -120,8 +148,11 @@ generate grm phrase
     where
       g = generate grm
 
--- | Generate a random sentence or phrase wiht a complete parse tree.
-generateTree :: Object -> Object -> Gen Object
+-- | Generate a random sentence or phrase with a complete parse tree.
+--
+-- >>> sample' $ generateTree simpleGrammar (S "noun-phrase")
+-- [(noun-phrase (article the) (noun ball)),(noun-phrase (article a) (noun table)),(noun-phrase (article a) (noun man)),(noun-phrase (article the) (noun ball)),(noun-phrase (article the) (noun woman)),(noun-phrase (article a) (noun woman)),(noun-phrase (article a) (noun ball)),(noun-phrase (article a) (noun table)),(noun-phrase (article a) (noun woman)),(noun-phrase (article a) (noun man)),(noun-phrase (article a) (noun ball))]
+generateTree :: Grammar -> Object -> Gen Object
 generateTree grm phrase
     | listp phrase = mapcar' g phrase
     | rewrites grm phrase /= Nil =
@@ -132,11 +163,11 @@ generateTree grm phrase
     where
       g = generateTree grm
 
--- | sample data
+-- | Generate a sample data.
 ex grm phrase = do
   xs <- sample' $ generate grm phrase
   return $ head xs
-
+-- | Generate a sample tree data.
 ex' grm phrase = do
   xs <- sample' $ generateTree grm phrase
   return $ head xs
@@ -161,6 +192,7 @@ randomElt choices
     | listp choices = elements (list2array choices)
     | otherwise = error ("randomElt expect choices but got: "  ++ show choices)
 
+-- | Big Grammar.
 bigGrammar = array2list [
               d "sentence" [["noun-phrase", "verb-phrase"]],
               d "noun-phrase" [["article", "adj*", "noun", "pp*"],
@@ -182,6 +214,7 @@ bigGrammar = array2list [
       d x xs = S x :. (S "->" :. array2list (map (array2list . map S) xs))
       w x xs = S x :. (S "->" :. array2list (map S xs))
 
+-- | Simple Grammar.
 simpleGrammar = array2list [
                  d "sentence" [["noun-phrase", "verb-phrase"]],
                  d "noun-phrase" [["article", "noun"]],
@@ -193,5 +226,3 @@ simpleGrammar = array2list [
     where
       d x xs = S x :. (S "->" :. array2list (map (array2list . map S) xs))
       w x xs = S x :. (S "->" :. array2list (map S xs))
-
-
